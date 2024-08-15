ee3e8e96fab155363e5be4882ee5c50f95d82794
rukal <rukalad@yandex.ru>
2024-08-15 11:23:22 +0300

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5*x&rwtek^5n+q+==slb!gbw1w%+bd+a9zoc1@(ldl9eed7le#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app.onrender.com']
+ALLOWED_HOSTS = ['geoff.pythonanywhere.com']
 
 
 # Application definition
